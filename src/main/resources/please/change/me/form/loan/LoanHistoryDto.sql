b14aa680230080a80eab7b2ebb66fac679caca8b
siosio <siosio12@gmail.com>
2017-09-22 08:51:37 +0900

    SQL規約に従うように修正

--- src/main/resources/please/change/me/form/loan/LoanHistoryDto.sql
+++ src/main/resources/please/change/me/form/loan/LoanHistoryDto.sql
@@ -1,14 +1,14 @@
 FIND_BY_LOAN_ID=
-select
-  kanji_name,
-  execution_date_time,
-  loan_appli_action_cd,
-  loan_appli_result_cd,
-  history_comment
-from
-  loan_application_history
-  inner join users
-    on user_id = executioner_id
-where
-  loan_appli_id = ?
-order by execution_date_time
+SELECT
+  KANJI_NAME,
+  EXECUTION_DATE_TIME,
+  LOAN_APPLI_ACTION_CD,
+  LOAN_APPLI_RESULT_CD,
+  HISTORY_COMMENT
+FROM
+  LOAN_APPLICATION_HISTORY
+  INNER JOIN USERS
+    ON USER_ID = EXECUTIONER_ID
+WHERE
+  LOAN_APPLI_ID = ?
+ORDER BY EXECUTION_DATE_TIME
