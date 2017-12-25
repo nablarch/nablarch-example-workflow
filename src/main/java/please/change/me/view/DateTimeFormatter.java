55edd9a9c2555dcbd9e226d1fe68aca02d1a4d0e
siosio <siosio12@gmail.com>
2017-12-25 14:52:22 +0900

    core-jdbcとuniversal-daoの変更を取り込み (#13)

--- src/main/java/please/change/me/view/DateTimeFormatter.java
+++ src/main/java/please/change/me/view/DateTimeFormatter.java
@@ -1,7 +1,9 @@
 package please.change.me.view;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 import javax.servlet.jsp.PageContext;
 
@@ -22,6 +24,9 @@ public class DateTimeFormatter implements ValueFormatter {
             return ((LocalDateTime) value).format(java.time.format.DateTimeFormatter.ofPattern(pattern));
         } else if (value instanceof LocalDate) {
             return ((LocalDate) value).format(java.time.format.DateTimeFormatter.ofPattern(pattern));
+        } else if (value instanceof Date) {
+            final SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+            return formatter.format((Date) value);
         }
         return value.toString();
     }
